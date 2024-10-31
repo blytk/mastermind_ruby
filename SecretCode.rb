6b44d8b8cdbc2c69ad153b54a9bcf792661f6f6a
Yago <dolmayan@gmail.com>
2024-10-31 23:41:40 +0100

    Create basic methods for class SecretCode

--- SecretCode.rb
+++ SecretCode.rb
@@ -1,26 +1,34 @@
 # Class for the secret code the player needs to guess
 class SecretCode
-  # color instance variables
-  attr_accessor :color1, :color2, :color3, :color4
+  # SecretCode class variables
+  # Represent the pins
   @@CORRECT_CORRECT_POSITION_PIN = 1
   @@CORRECT_WRONG_POSITION_PIN = 2
 
-  def initialize(color1, color2, color3, color4)
-    @color1 = color1
-    @color2 = color2
-    @color3 = color3
-    @color4 = color4  
+  # color instance variables
+  attr_accessor :colors, :pins
+
+  def initialize(color1, color2, color3, color4)  
     @colors = [color1, color2, color3, color4]
     @pins = []
   end
 
-  def guess_feedback(guess_color1, guess_color2, guess_color3, guess_color4)
-    # correct guess?
-    # correct guess in correct position?
-    if guess_color1 == @color1
-      pins.push(CORRECT_CORRECT_POSITION_PIN)
-    elsif colors.include?(color1)
-      pins.push(CORRECT_WRONG_POSITION_PIN)      
+  def create_feedback_pins(guess_array)
+    guess_array.each_with_index do |guess, guess_index|
+
+      # correct guess in correct position?
+      if guess == @colors[guess_index]
+        @pins.push(@@CORRECT_CORRECT_POSITION_PIN)
+      # correct guess but in different position?
+      elsif @colors.include?(guess)
+        @pins.push(@@CORRECT_WRONG_POSITION_PIN)
+      end
+
     end
   end
+
 end
+
+secret = SecretCode.new("white", "blue", "orange", "red")
+secret.create_feedback_pins(["red", "orange", "blue", "white"])
+p secret

60dbcade0881dcf21e358aeacbfe8cddb8f069f4
Yago <dolmayan@gmail.com>
2024-11-02 16:11:46 +0100

    Create method to control flow of the game / winning conditions

--- main.rb
+++ main.rb
@@ -2,13 +2,35 @@ require_relative "SecretCode"
 require_relative "Display"
 require_relative "Player"
 
+# Initialize secret code that the player must guess
 code = SecretCode.new("white", "white", "black", "black")
+# Create player
 player = Player.new()
-player.take_player_input
 
-code.create_feedback_pins(player.colors)
+def game_over?(player, code)
+    if player.guesses <= 0
+       true
+    end
+    
+    if player.colors == code.colors
+      Display.print_message("You have found the correct combination. You win.")
+      true
+    end
+end
 
-# player.send_selected_colors
+while !game_over?(player, code)
+  # Take input from the player
+  player.take_player_input
+  # Generate feedback with pins
+  code.create_feedback_pins(player.colors)
+  player.guesses -= 1
+  # If player guesses correct code in order, win
+end
+
+
+
+
+
 
 
 # Display.print_pins([1, 2, 2, 1])

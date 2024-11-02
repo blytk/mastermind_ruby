7646a1cdc72db739a3e9ad664a61ad46e0a658de
Yago <dolmayan@gmail.com>
2024-11-02 16:10:36 +0100

    Debug class Player

--- Player.rb
+++ Player.rb
@@ -16,34 +16,31 @@ class Player
     # Get input from the player
     def take_player_input
         get_color_input
-        Display.print_message("The colors you have selected are: #{@colors}")
+        Display.print_message("Turn ##{@@GUESSES - @guesses + 1}")
+        # Display.print_message("The colors you have selected are: #{@colors}")
+        Display.print_player_color_selection(@colors)
     end
 
     # Helper function to make sure input is correct / valid colors
     def get_color_input
         i = 1
+        # reset @colors array to get new input from the user
+        @colors = []
+        # Message
+        Display.print_message("Turn ##{@@GUESSES - @guesses + 1}")
+        # Logic to compare player input against code
         while @colors.length != SecretCode.number_of_colors
             Display.print_message("Enter color number #{i}: ")
             color = gets.chomp
             color = color.downcase
             if !SecretCode.valid_colors.include?(color)
                 puts "Color not available"
-                Display.print_message("Avaiable colors are: #{SecretCode.valid_colors}")
+                Display.print_message("Available colors are: #{SecretCode.valid_colors}")
             else 
                 @colors.push(color)
                 i += 1
             end
         end
     end
-
-    # Send selected colors to SecretCode
-    def send_selected_colors
-        if @colors.length == SecretCode.number_of_colors
-            SecretCode.create_feedback_pins(@colors)
-        else
-            Display.print_message("Incorrect number of colors in your selection")
-        end      
-    end
-
 end
 
